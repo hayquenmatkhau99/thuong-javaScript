762888ed48ed6e914977f47be4358eef634a7f25
Nguyen Thi Thuong <thuongnt1.bhsoft@gmail.com>
2022-06-30 22:53:34 +0700

    Update check input form

--- form.js
+++ form.js
@@ -23,4 +23,30 @@ function check_step_one() {
         birth_date.classList.add('request');
         document.getElementsByClassName('first_date')[0].classList.add('showerror');
     }
-} 
+    if (document.querySelector('input[name="input_radio"]:checked') == null) {
+        document.getElementsByClassName('radio')[0].classList.add('showerror');
+        // get_child_text()
+    }
+    var email = document.getElementById('ff_50_email');
+    if (email.value == "") {
+        var email_lable = document.getElementById('email');
+        get_child_text(email_lable)
+    }
+    var phone = document.getElementById('ff_50_numeric-field');
+    if (phone.value == "") {
+        var phone_error = document.getElementById('phone');
+        get_child_text(phone_error)
+    }
+}
+function get_child_text(parrentNode) {
+    if (parrentNode) {
+        textNode = document.createTextNode(
+            'This field is required'
+        );
+        pTag = document.createElement("p");
+        pTag.appendChild(textNode);
+        pTag.classList.add('showerror');
+        parrentNode.appendChild(pTag);
+
+    }
+}
